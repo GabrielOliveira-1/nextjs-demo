9783c6a42700693b37ddcdffcf4783c4a259f066
gabriel427131 <gabriel.d.oliveira1@hotmail.com>
2024-01-19 18:42:09 -0700

    Update news detail page
    - Extract dynamic parameter values
    
    - Link to access the pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,12 @@
+import { useRouter } from "next/router";
+
 // domain.com/news/detail-page
 
 function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
   return <h1>Detail Page</h1>;
 }
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,22 @@
+import Link from "next/link";
+import { Fragment } from "react";
+
 // domain.com/news
 
 function NewsPage() {
-  return <h1>News Page</h1>;
+  return (
+    <Fragment>
+      <h1>News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS Framework in a Nutshell
+          </Link>
+        </li>
+        <li>Other Frameworks</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default NewsPage;
